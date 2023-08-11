e3851ac6c62b86964ee4db5cf8ac60920a88af58
Anders Bo Pedersen <ABP@ao.dk>
2023-08-11 14:37:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+
+namespace AsymmetriskKrypteringRsaParameterModtager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //virker ikke, den smider en exception når den modtager den krypterede data
+
+            var rsaParameters = new RSAParameterKey();
+            string encryptedText;
+
+            try
+            {
+                Console.WriteLine("RSA parameter");
+                Console.WriteLine();
+
+                Console.WriteLine("Indset privat nøgle D");
+                rsaParameters.privateKey.D = ByteArrayFromReadLine();
+                Console.WriteLine("Indset privat nøgle DP");
+                rsaParameters.privateKey.DP = ByteArrayFromReadLine();
+                Console.WriteLine("Indset privat nøgle DQ");
+                rsaParameters.privateKey.DQ = ByteArrayFromReadLine();
+                Console.WriteLine("Indset privat nøgle Exponent");
+                rsaParameters.privateKey.Exponent = ByteArrayFromReadLine();
+                Console.WriteLine("Indset privat nøgle InverseQ");
+                rsaParameters.privateKey.InverseQ = ByteArrayFromReadLine();
+                Console.WriteLine("Indset privat nøgle Modulus");
+                rsaParameters.privateKey.Modulus = ByteArrayFromReadLine();
+                Console.WriteLine("Indset privat nøgle P");
+                rsaParameters.privateKey.P = ByteArrayFromReadLine();
+                Console.WriteLine("Indset privat nøgle Q");
+                rsaParameters.privateKey.Q = ByteArrayFromReadLine();
+                Console.WriteLine();
+
+                Console.WriteLine("Indset det krypterede data:");
+                encryptedText = Console.ReadLine();
+
+                var rsaDecrypted = rsaParameters.Decrypt(Encoding.UTF8.GetBytes(encryptedText));
+
+                var rsaDecryptedText = Encoding.UTF8.GetString(rsaDecrypted);
+
+                Console.WriteLine();
+                Console.WriteLine("Dekryptered tekst: \r\n" + rsaDecryptedText);
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Der skete den fejl!");
+            }
+        }
+
+
+        private static byte[] ByteArrayFromReadLine()
+        {
+            string inputData = Console.ReadLine();
+            string[] splitString = inputData.Split('-');
+            byte[] privateKeyData = new byte[splitString.Length];
+
+            privateKeyData = Encoding.UTF8.GetBytes(inputData);
+
+            return privateKeyData;
+        }
+    }
+}
---
+++ RSAParameterKey.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsymmetriskKrypteringRsaParameterModtager
+{
+    public class RSAParameterKey
+    {
+        public RSAParameters privateKey;
+        public RSAParameters publicKey;
+
+        /// <summary>
+        /// Method for decrypting the cipher,
+        /// Create byte arrays to hold the decrypted text,
+        /// Create a new instance of RSA,
+        /// Import the RSA privateKey information,
+        /// Decrypt the byte array and sets OAEP padding
+        /// </summary>
+        /// <param name="textToEncrypt"></param>
+        /// <returns></returns>
+        public byte[] Decrypt(byte[] textToEncrypt)
+        {
+            byte[] decryptedText;
+
+            try
+            {
+                var rsa = RSA.Create();
+
+                rsa.ImportParameters(privateKey);
+                decryptedText = rsa.Decrypt(textToEncrypt, RSAEncryptionPadding.OaepSHA256);
+
+                return decryptedText;
+            }
+            catch (CryptographicException e)
+            {
+                Debug.WriteLine(e.Message);
+
+                return null;
+            }
+        }
+    }
+}
